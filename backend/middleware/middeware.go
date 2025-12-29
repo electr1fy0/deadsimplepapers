432e47c49b1463bc08837eb394bee9f320c242cd
electr1fy0 <electr1fy0@icloud.com>
2025-12-29 15:31:26 +0530

    add more valid domains for cors

--- backend/middleware/middeware.go
+++ backend/middleware/middeware.go
@@ -2,16 +2,27 @@ package middleware
 
 import (
 	"net/http"
-	"os"
 )
 
-var origin = os.Getenv("ORIGIN")
+var allowedOrigins = map[string]bool{
+	"http://papers.ayushpandey.xyz":       true,
+	"https://vitpapers.vercel.app":        true,
+	"https://deadsimplepapers.vercel.app": true,
+}
 
 func CORSMiddleware(next http.Handler) http.Handler {
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, apiKey")
+		origin := r.Header.Get("origin")
+
+		if allowedOrigins[origin] {
+			w.Header().Set("Vary", "Origin")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, apiKey")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		}
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
